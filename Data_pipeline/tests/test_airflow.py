97088ce1d81040f538401b4d03577c9b2d896a88
AnjanaDevan27 <anjanacdm2@gmail.com>
2025-03-29 19:00:45 -0400

    Updated files

---
+++ Data_pipeline/tests/test_airflow.py
@@ -0,0 +1,18 @@
+import pytest
+from airflow.models import DagBag
+def test_airflow_dag_loading():
+    """
+    Test that Airflow successfully loads DAGs from the project.
+    """
+    dag_bag = DagBag(dag_folder="Data_pipeline/dags", include_examples=False)
+    
+    # If there are import errors, print them and fail the test.
+    if dag_bag.import_errors:
+        pytest.fail(f"DAG import errors: {dag_bag.import_errors}")
+    
+    # Debug: Print out the IDs of loaded DAGs.
+    loaded_dags = list(dag_bag.dags.keys())
+    print("Loaded DAGs:", loaded_dags)
+    
+    # Check that at least one DAG is loaded.
+    assert dag_bag.dags, "No DAGs were loaded. Please check your DAG files."
